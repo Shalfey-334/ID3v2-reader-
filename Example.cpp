5a97f355edba91944e74202bade0a8c77a2e791b
Shalfey-334 <54683058+Shalfey-334@users.noreply.github.com>
2019-11-06 21:50:09 +0500

    Add files via upload

---
+++ Example.cpp
@@ -0,0 +1,16 @@
+﻿
+#include <iostream>
+
+#include "File_object.h"
+#include "ID3_reader.h"
+
+
+int main()
+{
+	File_object s("Adress");
+	ID3_reader der(&s);
+	der.say_all_frames();
+	system("PAUSE");
+	return 0;
+}
+
